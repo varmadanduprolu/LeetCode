2096ef2b2a58ac525292b01a1e6eabe5907355da
Madhav <varmadhanduprolu@gmail.com>
2023-09-08 13:04:12 +0530

    adding new files

---
+++ src/com/varma/leetcode/arrays/SignOfTheProductOfAnArray.java
@@ -0,0 +1,27 @@
+package com.varma.leetcode.arrays;
+
+import java.util.Scanner;
+
+public class SignOfTheProductOfAnArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int len=sc.nextInt();
+        int[] nums=new int[len];
+        for (int i = 0; i < nums.length ; i++) {
+            nums[i]=sc.nextInt();
+        }
+        System.out.println(arraySign(nums));
+    }
+
+    private static int arraySign(int[] nums) {
+        int res=0;
+        for (int i = 0; i < nums.length; i++) {
+           if (nums[i]==0)
+               return 0;
+           else if (nums[i]<0)
+               res++;
+        }
+     return (res%2==0)?1:-1;
+
+    }
+}
