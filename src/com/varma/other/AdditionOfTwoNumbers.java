5d9401d389b776aa2e463fd11ae8844856c6984e
Madhav <varmadhanduprolu@gmail.com>
2023-11-09 00:01:03 +0530

    adding new problem

---
+++ src/com/varma/other/AdditionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.varma.other;
+
+public class AdditionOfTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println("talkig to you ");
+    }
+}
+/***
+    Edit++
+    Eclipse
+    IntelIj
+    netbeans
+    Pycharm
+ ***/
